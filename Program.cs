195d015a5b25f1c17b3fcad91a3338e5cbd594d7
Mohamed Fathy <codetwostart@gmail.com>
2019-10-10 22:17:25 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace assignmentandbitwiseoperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //+=, -=, *=, /=
+            int a = 3;
+            //a++;
+            //a = a + 2;//
+            /*
+            a += 2;
+            a *= 3;
+            Console.WriteLine(a);
+             */
+             Console.WriteLine(1 | 2);//3
+             Console.WriteLine(1 & 2);//0
+
+
+        }
+    }
+}
